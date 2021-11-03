55afbfc7dc3dd379ddcdead22f9e872e2770c20e
igorlizyakin <igor.lizyakin@gmail.com>
2021-11-03 19:54:23 +0300

    New4

--- Archiv3.py
+++ Archiv3.py
@@ -8,9 +8,10 @@ from os.path import exists
 
 def parse_arg():
     my_parser = argparse.ArgumentParser()
-    my_parser.add_argument('-d','--directory')
-
-
+    my_parser.add_argument('-d','--directory', action='store', required=True, help='Обязательный аргумент')
+    my_parser.add_argument('-o', '--output', action='store', required=True, help='Обязательный аргумент')
+    my_parser.add_argument('-a', '--algo', action='store', default='gztar', help='Указания указать формать сжатия')
+    my_parser.add_argument('-j', '--journal', action='store', default='journal.csv', help='Путь до создаваемого файла')
     args = my_parser.parse_args()
     return args.directory, args.output, args.algs, args.jornal
 #
@@ -24,11 +25,41 @@ def access(path, flag, text):
     else:
         return True
 
-    def is_radable(path):
+    def is_readable(path):
         return access(path, os.R_OK, 'Отсутствуют права на чтение')
 
-    raise
-archive_name = shutil.make_archive(archive_name, algo, abs_path_to_dir)
+    def is_writable(path):
+        return acces(path, os.R_OK, 'Отсутствуют права на запись')
+
+    def make_archive(dir, out, algo, journal):
+        init_journal(journal)
+        abs_path_to_dir = os.path.abspath(dir)
+        basename = os.path.basename(abs_path_to_dir)
+        abs_path_to_dir = os.path.abspath(out)
+        date_uts = datetime.now(timezone.utc).strftime('%d-%m-%Y_%H-%M-%S')
+        archive_name = f'{basename}_{date_uts}'
+        try:
+            if not (is_readable(abs_path_to_dir) & is_writable(abs_path_to_dir))
+                raise
+            archive_name = shutil.make_archive(archive_name, algo, abs_path_to_dir)
+            abs_path_to_dir = shutil.move(archive_name, abs_path_to_out)
+            status = "success"
+        except Exception as ex:
+            status = "fail"
+            sys.stderr.write(str(ex))
+        if not exists(abs_path_to_out) or (status == 'fail'):
+            path_to_out = ''
+        else:
+            path_to_out = abs_path_to_out
+            sys.stdout.write('Архив собран и записан: '+ path_to_out+ '\n')
+        write_jornal(abs_path_to_dir, path_to_out, journal, status)
+
+
+
+
+
+
+#archive_name = shutil.make_archive(archive_name, algo, abs_path_to_dir)
 #
 #
 #
@@ -51,4 +82,3 @@ def write_jornal(dir,out,journal,status):
 if __name__ == '__main__':
     dir1, out, algo, journal = parse_arg()
     make_archive(dir1,out,algo,journal)
-    
